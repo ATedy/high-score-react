a9d98628f96d0b4409f5fb4514cc6b411b161e8c
Amanuel <weditedi25@gmail.com>
2021-03-24 19:00:19 +0000

    Level-5 finished (World Score Ranking)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import CountryName from "./CountryName";
 import {allCountryScores} from "./countryScoreData";
 import PlayerScore from "./PlayerScore";
 
 function App() {
+  const [countryView, setCountryView] = useState(false);
+
+  function handler() {
+    setCountryView(!countryView);
+  }
+  const highScore = allCountryScores.map((country) => country.scores).flat();
+  console.log(highScore);
   allCountryScores.sort((a, b) => {
     // sorting the countries based on their names
     let x = a.name.toLowerCase();
@@ -21,12 +28,17 @@ function App() {
   return (
     <div className="card">
       <h3 className="text-center">High Scores per Country</h3>
-      {allCountryScores.map((country) => (
-        <div className="card-header">
-          <CountryName oneCountry={country} />
-          <PlayerScore oneCountryScore={country.scores} />
-        </div>
-      ))}
+      <button onClick={handler}>All-Country-HighScore</button>
+      {countryView ? (
+        allCountryScores.map((country) => (
+          <div className="card-header">
+            <CountryName oneCountry={country} />
+            <PlayerScore oneCountryScore={country.scores} />
+          </div>
+        ))
+      ) : (
+        <PlayerScore oneCountryScore={highScore} />
+      )}
     </div>
   );
 }
